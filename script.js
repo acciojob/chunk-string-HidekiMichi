4a622062032d4b62591100cc9217782a416ee4a0
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2024-01-30 14:40:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 function stringChop(str, size) {
   // your code here
+	let n=str.length;
+	if(n<=size){
+		return str;
+	}
+	
+	let ans=[];
+	let temp="";
+	
+	let quo=Math.floor(n/size);
+	let idx=0;
+	
+	for(let i=0;i<quo;i++){
+		let j=0;
+		while(j<size){
+			temp+=str.charAt(idx++);
+			j++;
+		}
+		ans.push(temp);
+		temp="";
+	}
+	while(idx<n){
+		temp+=str.charAt(idx++);
+	}
+	ans.push(temp);
+	return ans;
 }
 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+alert(stringChop(str, size));   
